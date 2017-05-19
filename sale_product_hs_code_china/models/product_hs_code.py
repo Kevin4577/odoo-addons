5d873d92100492f2625d5b71516764e70606a3e3
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-05-19 15:26:43 +0530

    [IMP]sale_product_hs_code_china: move required parameter from model level to view level.

--- sale_product_hs_code_china/models/product_hs_code.py
+++ sale_product_hs_code_china/models/product_hs_code.py
@@ -8,6 +8,6 @@ from odoo import fields, models
 class ProductHsCode(models.Model):
     _inherit = "product.hs.code"
 
-    cn_name = fields.Char('Name (CN)', required=True, help='Chinese Name')
+    cn_name = fields.Char('Name (CN)', help='Chinese Name')
     tax_id = fields.Many2one('account.tax', 'Rebate Rate',
-                             required=True, help='Tax Rebate Rate')
+                             help='Tax Rebate Rate')
