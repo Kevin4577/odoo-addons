ab8c59e2a1a3ba348279c18996d8bf73c86bee00
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-05-25 16:36:52 +0530

    [IMP]Improved travis.

--- sale_product_hs_code/models/product_template.py
+++ sale_product_hs_code/models/product_template.py
@@ -2,7 +2,7 @@
 # © 2017 Elico corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)..
 
-from odoo import fields, models, _
+from odoo import fields, models
 
 
 class ProductTemplate(models.Model):
