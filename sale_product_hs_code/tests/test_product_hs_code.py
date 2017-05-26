cf1b3d14c0885d72937248c2966a7bc83cef2efa
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-05-26 16:27:58 +0530

    [ADD]Added unit test cases in product hs code module.

---
+++ sale_product_hs_code/tests/test_product_hs_code.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import common
+from odoo.exceptions import ValidationError
+
+
+class TestProductHSCode(common.TransactionCase):
+
+    def setUp(self):
+        super(TestProductHSCode, self).setUp()
+        self.prod_hs_code_obj = self.env['product.hs.code']
+        self.prod_hs_code = self.prod_hs_code_obj.create({
+                      'hs_code': 'HSCode1', 'name': 'Test1'})
+
+    def test_check_hs_code(self):
+        "Test product code based on sequence"
+        with self.assertRaises(ValidationError):
+            self.prod_hs_code2 = self.prod_hs_code.copy({
+                'hs_code': 'HSCode1'
+            })
