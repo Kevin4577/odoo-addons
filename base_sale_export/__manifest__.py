c86793c8572e613c93179bbd4250c0e888819dc6
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-06-06 12:27:00 +0530

    [IMP]Improved description

--- base_sale_export/__manifest__.py
+++ base_sale_export/__manifest__.py
@@ -6,10 +6,7 @@
     'name': 'Base Sale Export',
     'version': '10.0.1.0.0',
     'category': 'Stock',
-    'summary': """The new module 'base_sale_export' would provide methods
-    for sale report modules with the data. Those methods would be reused
-    by several modules, in order to improve the maintainability of project
-    modules.""",
+    'summary': """Sales Export trading common methods and information.""",
     'author': "Elico Corp",
     'website': 'https://www.elico-corp.com',
     'license': 'AGPL-3',
