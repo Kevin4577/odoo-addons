298880c45e7468c89c70f474a81f1c33bbdfe80b
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-06-13 12:06:19 +0530

    [ADD]Added new module stock picking partner invisible

---
+++ stock_picking_partner_invisible/models/stock_picking.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models, fields
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    partner_name = fields.Char(related='partner_id.name', string='Partner')
+    user_group = fields.Boolean(compute='compute_user_group', string="User "
+                                                                     "group")
+
+    @api.multi
+    def compute_user_group(self):
+        self.user_group = self.env.user.has_group(
+            'stock_picking_partner_invisible.partner_invisible')
