e0e4de51463a8fd38db5aee26104c0f9d2692239
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2017-06-14 13:43:27 +0530

    [IMP]AddedOnchange and improved attrs

--- trading_stock_shipping/models/stock_picking.py
+++ trading_stock_shipping/models/stock_picking.py
@@ -2,7 +2,7 @@
 # © 2017 Elico Corp (www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
@@ -12,3 +12,9 @@ class StockPicking(models.Model):
 
     ship_info_id = fields.Many2one("shipping", 'Shipping Information',
                                    help="Shipping Information")
+
+    @api.onchange('custom_check')
+    def onchange_custom_check(self):
+        for rec in self:
+            if not rec.custom_check:
+                rec.ship_info_id = False
