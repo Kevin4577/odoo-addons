f8d901d2f703be38a5ff54c4237e6ee1830fc6f9
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-06-15 13:04:51 +0530

    [FIX][IMP][10.0][Issue20][BR000756]: Added fields_view_get method in product template and added unit test cases.

---
+++ sale_product_hs_code_china/tests/test_product_hs_code_china.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico corp (www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import common
+from odoo.exceptions import ValidationError
+
+
+class TestProductHSCodeChina(common.TransactionCase):
+
+    def setUp(self):
+        super(TestProductHSCodeChina, self).setUp()
+        self.prod_temp_obj = self.env['product.template']
+        self.view_id = self.env.ref('product.'
+                                    'product_template_only_form_view').id
+
+    def test_fields_view_get(self):
+        """Test fields view get method for invisible hs name"""
+        self.prod_temp_obj.fields_view_get(view_id=self.view_id)
