aa0fc3372d69bb1b68485a8d6fb821f55c582104
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-06-20 17:47:01 +0530

    [ADD] Added a new invoice_export_outbound_order_printout module

---
+++ trading_stock_delivery_note_by_lot/report/trading_stock_delivery_note_by_lot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from odoo import _
+from odoo.addons.report_py3o.models import py3o_report
+from odoo.exceptions import ValidationError
+
+
+@py3o_report.py3o_report_extender(
+        report_xml_id='trading_stock_delivery_note_by_lot.'
+        'trading_stock_delivery_note_by_lot_py3o')
+def render_report_with_data(report_xml_id, ctx):
+    """This function get the data from sale order of specific stock
+    picking, and sum the carton quantity and weight, in order to render the
+    ods template with necessary data."""
+    stock_picking_list = ctx['objects']
+    if len(stock_picking_list.mapped('partner_id').ids) > 1:
+        raise ValidationError(_('Please check whether all delivery'
+                                'orders belong to one customer'))
+    base_invoice_export_obj = stock_picking_list.env['trading.invoice']
+    if stock_picking_list.sale_id:
+        ctx.update(base_invoice_export_obj.
+                   get_product_lot_list_per_sale_order(stock_picking_list))
+    else:
+        raise ValidationError(_('Please check whether this stock picking'
+                                'was generated from sale order.'))
