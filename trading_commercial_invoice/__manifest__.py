4829afa2afac24851fefa80dbb29f9c8acdeed66
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-06-20 17:11:22 +0530

    [ADD][10.0][TS0017YG-TS06][BR000772][T21330]: Added trading_commercial_invoice module.

---
+++ trading_commercial_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico corp (https://www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Trading Commercial Invoice',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """The new module 'trading_commercial_invoice' generate the xls
+    or ods report from ods template file with each order lines of product
+    inside account invoice.""",
+    'author': "Elico Corp",
+    "support": "support@elico-corp.com",
+    'website': 'https://www.elico-corp.com',
+    'license': 'AGPL-3',
+    'depends': [
+        'trading_invoice',
+        'report_py3o',
+    ],
+    'data': [
+        'report/trading_commercial_invoice.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
