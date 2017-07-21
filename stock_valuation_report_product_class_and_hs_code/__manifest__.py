c107e11054cbd28f7b0b68b6695926199eedbdb0
zhouhao <zhouhao19931002@hotmail.com>
2017-07-21 15:37:53 +0800

    [FIX]base on commit

--- stock_valuation_report_product_class_and_hs_code/__manifest__.py
+++ stock_valuation_report_product_class_and_hs_code/__manifest__.py
@@ -4,6 +4,7 @@
 {
     'name': 'Stock Valutaion Report With Product Class and HS code',
     'version': '10.0.1.0.0',
+    'category': 'Stock',
     "summary": """Stock Valutaion Report With Product Class and HS code""",
     "support": "support@elico-corp.com",
     'depends': [
