e37ded35f6376b2cbf679fbb7f467853baacece0
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-07-27 12:56:24 +0530

    [WIP][10.0][TS0024YG][BR000764][T21499] Improved Travis.

--- purchase_order_line_received_quantity_refund/models/purchase.py
+++ purchase_order_line_received_quantity_refund/models/purchase.py
@@ -2,7 +2,7 @@
 # © 2017 Elico Corp (https://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
