5adc2ebb7956601aa6d3c64d9a8af6bc3c1946f2
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2017-08-01 16:13:01 +0530

    [WIP][10.0][TS0024YG][BR000764][T21499] Removed unused import statement.

--- purchase_order_line_received_quantity_refund/models/purchase.py
+++ purchase_order_line_received_quantity_refund/models/purchase.py
@@ -2,8 +2,7 @@
 # © 2017 Elico Corp (https://www.elico-corp.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
-import odoo.addons.decimal_precision as dp
+from odoo import api, models
 
 
 class PurchaseOrderLine(models.Model):
