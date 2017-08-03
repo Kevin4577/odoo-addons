8e0375f112d18aaf4fc52c9340af66af5c506130
Nikul Chaudhary <nikul.chaudhary.serpentcs@gmail.com>
2017-08-03 18:27:42 +0530

    [ADD] trading_vendor_billing_confirmation

---
+++ trading_vendor_billing_confirmation/README.rst
@@ -0,0 +1,51 @@
+ .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+====================================
+Trading Vendor Billing Confirmation
+====================================
+
+The module 'trading_vendor_billing_confirmation' will inherit the model 'account.invoice.line', and add reference
+of product, vendor, customer product code. It allows user to generate billing confirmation report, based on the product,
+invoiced quantity, total price of each account invoice line
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/Elico-Corp/yugong-odoo/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
+
+Credits
+=======
+
+Images
+------
+
+* Elico Corp: `Icon <https://elico-corp.com/logo.png>`_.
+
+Contributors
+------------
+
+* Eric Caudal <eric.caudal@elico-corp.com>
+* Victor Martin <victor.martin@elico-corp.com>
+* Zhou Hao <zhou.hao@elico-corp.com>
+
+Maintainer
+----------
+
+.. image:: https://www.elico-corp.com/logo.png
+   :alt: Elico Corp
+   :target: https://www.elico-corp.com
+
+This module is maintained by Elico Corporation.
+
+Elico Corporation offers consulting services to implement open source management
+software in SMEs, with a strong involvement in quality of service.
+
+Our headquarters are located in Shanghai and Singapore servicing customers from
+Greater China and Asia Pacific.
+
+Contact information: `Sales <contact@elico-corp.com>`__
---
+++ trading_vendor_billing_confirmation/models/account_invoice.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# © 2017 Elico Corp (https://www.elico-corp.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    @api.depends('purchase_line_id.procurement_ids.move_dest_id.state')
+    def _compute_state_sale(self):
+        """This function will compute state from the stock move of
+        related sale order."""
+        for line in self:
+            if line.purchase_line_id:
+                if line.purchase_line_id.procurement_ids:
+                    if line.purchase_line_id.procurement_ids.move_dest_id.state == 'done':
+                        line.state_sale = 'done'
+                    else:
+                        line.state_sale = 'waiting'
+                else:
+                    line.state_sale = ''
+
+    vendor_reference = fields.Char(
+        'Vendor Reference',
+        related='purchase_line_id.vendor_reference',
+        readonly=True,
+        help='Internal Reference of vendor of purchase order.'
+    )
+
+    vendor_order = fields.Char(
+        'Order Vendor',
+        related='purchase_line_id.partner_id.name',
+        readonly=True,
+        help='Name of vendor of purchase order.'
+    )
+
+    customer_reference = fields.Char(
+        'Customer Reference',
+        related='purchase_line_id.order_id.partner_id.ref',
+        readonly=True,
+        help='Internal Reference of customer of purchase order.'
+    )
+
+    vendor_order_reference = fields.Char(
+        'Vendor Order Reference',
+        related='purchase_line_id.vendor_order_reference',
+        readonly=True,
+        help='Source document of vendor order reference of purchase order.'
+    )
+
+    purchase_order_reference = fields.Char(
+        'Purchase Order Reference',
+        related='purchase_line_id.purchase_order_reference',
+        readonly=True,
+        help='The purchase order number of related purchase order.'
+    )
+
+    customer_product_code = fields.Char(
+        'Customer Product Code',
+        related='product_id.customer_product_code',
+        readonly=True,
+        help='The customer product code of specific product.'
+    )
+
+    qty_order = fields.Float(
+        'Order Quantity',
+        related='purchase_line_id.product_qty',
+        readonly=True,
+        help='The original product quantity of related purchase order line.'
+    )
+
+    price_subtotal_order = fields.Monetary(
+        'Total Price Order',
+        related='purchase_line_id.price_subtotal',
+        readonly=True,
+        help='The total amount of related purchase order line.'
+    )
+
+    state_sale = fields.Char(
+        'Sale Order State',
+        compute='_compute_state_sale',
+        readonly=True,
+        store=True,
+        help='The state of stock move of related sale order.'
+    )
+
+    hs_cn_name = fields.Char(
+        'Product HS Code CN Name',
+        related='product_id.hs_cn_name',
+        readonly=True,
+        help='The state of stock move of related sale order.'
+    )
