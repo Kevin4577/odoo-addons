a1d3a5e904b045aba5353112eef4440645920ed9
zhouhao <zhouhao19931002@hotmail.com>
2017-08-09 12:27:26 +0800

    [IMP]UAT

--- trading_sale/tests/test_trading_sale.py
+++ trading_sale/tests/test_trading_sale.py
@@ -12,6 +12,11 @@ class TestTradingSale(common.TransactionCase):
         product_product = self.env['product.product']
         self.product_uom = self.env.ref('product.product_uom_unit')
         self.partner = self.env.ref('base.res_partner_1')
+        self.product_packaging_model = self.env['product.packaging']
+        self.production_lot_model = self.env['stock.production.lot']
+        self.pack_operation_model = self.env['stock.pack.operation']
+        self.pack_operation_lot_model = self.env['stock.pack.operation.lot']
+        self.pack_obj = self.env['stock.quant.package']
         self.partner.write({'ref': 'test_reference'})
         self.trading_sale = self.env['trading.sale']
         self.product_hs = product_hs_code.create({
@@ -26,6 +31,13 @@ class TestTradingSale(common.TransactionCase):
             'product_hs_code_id': self.product_hs.id,
             'default_code': 'Test Default Code'
         })
+        self.shipping_model = self.env['shipping']
+        self.shipping_id = self.shipping_model.create({
+            'name': 'Test Shipping',
+            'ship_from': self.partner.id,
+            'ship_to': self.partner.id,
+            'ship_by': 'Test Ship By',
+        })
         self.so = self.env['sale.order'].create({
             'partner_id': self.partner.id,
             'partner_invoice_id': self.partner.id,
@@ -39,6 +51,45 @@ class TestTradingSale(common.TransactionCase):
                 'price_unit': self.product1.list_price,
             })],
         })
+        self.so.action_confirm()
+        self.so.picking_ids.action_confirm()
+        self.so.picking_ids[0].write({
+            'ship_info_id': self.shipping_id.id
+        })
+        self.so.picking_ids.action_assign()
+        self.so.picking_ids.force_assign()
+        self.pack1 = self.pack_obj.create({
+            'name': 'Test PACKINOUTTEST'
+        })
+        self.so.picking_ids. \
+            pack_operation_ids[0].result_package_id = self.pack1
+        self.so.picking_ids.pack_operation_product_ids.write({
+            'qty_done': 5.0
+        })
+        self.packaging_id = self.product_packaging_model.create({
+            'name': 'Test box of 10'
+        })
+        self.so.picking_ids.pack_operation_product_ids[0]. \
+            result_package_id.write({
+                'packaging_id': self.packaging_id.id
+            })
+        self.lot1 = self.production_lot_model.create({
+            'product_id': self.product_4.id,
+            'name': 'Test LOT1',
+            'volume': 10.0,
+            'carton_qty': 5.0
+        })
+        pack_opt = self.pack_operation_model. \
+            search([('picking_id', '=', self.sale_order.picking_ids[0].id)],
+                   limit=1)
+        self.pack_operation_lot_model.create({
+            'operation_id': pack_opt.id,
+            'lot_id': self.lot1.id,
+            'qty': 5.0,
+            'volume': 10.0,
+            'carton_qty': 5.0,
+        })
+        self.so.picking_ids.do_new_transfer()
 
     def test_get_product_hs_code_list(self):
         """This function should check the filter reporting element of hs
