566c908e7d94b4cb79c89dc4def496237e5673f2
Hulk <juebanliuhao@163.com>
2018-04-27 15:08:38 +0800

    fix word error, add log

--- trading_purchase_delivery_note_by_lot/report/trading_purchase_delivery_note_by_lot.py
+++ trading_purchase_delivery_note_by_lot/report/trading_purchase_delivery_note_by_lot.py
@@ -4,6 +4,9 @@
 from odoo import _
 from odoo.addons.report_py3o.models import py3o_report
 from odoo.exceptions import ValidationError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 
 @py3o_report.py3o_report_extender(
@@ -13,16 +16,20 @@ def render_report_with_data(report_xml_id, ctx):
     """This function get the data from sale order of specific stock
     picking, and sum the carton quantity and weight, in order to render the
     ods template with necessary data."""
-    stock_picking_list = ctx['objects']
-    if len(stock_picking_list.mapped('partner_id').ids) > 1:
-        raise ValidationError(_('Please check whether all delivery '
-                                'orders period belong to one customer'))
-    base_invoice_export_obj = stock_picking_list.env['trading.invoice']
-    if not stock_picking_list.filtered(lambda stock_picking:
-                                       not stock_picking.pruchase_id):
-        data = base_invoice_export_obj.get_product_lot_list_per_sale_order(
-            stock_picking_list)
-        ctx.update(data)
-    else:
-        raise ValidationError(_('Please check whether this stock picking '
-                                'was generated from purchase order.'))
+    try:
+        stock_picking_list = ctx['objects']
+        if len(stock_picking_list.mapped('partner_id').ids) > 1:
+            raise ValidationError(_('Please check whether all delivery '
+                                    'orders period belong to one customer'))
+        base_invoice_export_obj = stock_picking_list.env['trading.invoice']
+        if not stock_picking_list.filtered(lambda stock_picking:
+                                           not stock_picking.purchase_id):
+            data = base_invoice_export_obj.get_product_lot_list_per_sale_order(
+                stock_picking_list)
+            ctx.update(data)
+        else:
+            raise ValidationError(_('Please check whether this stock picking '
+                                    'was generated from purchase order.'))
+    except Exception as e:
+        _logger.error(e)
+        raise e
